2d964c7d18ef3b7f82ae5cb2acae0ecb9f435310
minhtoan-nmt <nguyenminhtoan204@gmail.com>
2026-01-26 10:14:25 +0700

    add 2 buttons on navbar

---
+++ frontend/src/component/homepage/Button.jsx
@@ -0,0 +1,7 @@
+export default function Button({content}) {
+    return (
+        <button className="bg-gray-700 text-white px-5 py-2 rounded-lg text-2xl">
+            {content}
+        </button>
+    )
+}
--- frontend/src/component/homepage/Homepage.jsx
+++ frontend/src/component/homepage/Homepage.jsx
@@ -1,10 +1,15 @@
 import logo from './../../assets/cleannie_studio_logo_-removebg-preview.png'
 import bgImage from './../../assets/snapedit_1769088011261.jpeg'
+import Button from './Button'
 
 export default function Homepage() {
     return (<div id='homepage' className='w-screen h-screen'>
         <div id="navbar" className='flex flex-row justify-between'>
-            <img src={logo} alt="Logo" className='overflow-hidden height-64 width-48 object-fit'/>
+            <div className='flex flex-row gap-4 items-center'>
+                <img src={logo} alt="Logo" className='overflow-hidden height-64 width-48 object-fit'/>
+                <div><Button content={"Scan nhanh"} /></div>
+                <div><Button content={"Mua sắm ngay"} /></div>
+            </div>
             <div className='flex flex-row jusitfy-between gap-10 items-center relative right-10'>
                 <p className='text-lg'>Sản phẩm</p>
                 <p className='text-lg'>Bộ sưu tập</p>
