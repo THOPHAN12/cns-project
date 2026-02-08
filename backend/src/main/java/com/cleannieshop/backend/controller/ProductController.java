4c3d38f1d6d0f643c9facdefc2779f7c1675178d
minhtoan-nmt <nguyenminhtoan204@gmail.com>
2026-02-08 10:26:28 +0700

    product page fetching from db

--- backend/src/main/java/com/cleannieshop/backend/controller/ProductController.java
+++ backend/src/main/java/com/cleannieshop/backend/controller/ProductController.java
@@ -13,14 +13,15 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 
 @RestController
 @RequestMapping("api/products")
+@CrossOrigin(origins = "http://localhost:5173")
 public class ProductController {
     @Autowired
     private ProductService productService;
