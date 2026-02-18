43d667b710acd6b3e3681198d24287f997397895
minhtoan-nmt <nguyenminhtoan204@gmail.com>
2026-02-18 21:38:12 +0700

    add cross origin to email controller

--- backend/src/main/java/com/cleannieshop/backend/controller/EmailController.java
+++ backend/src/main/java/com/cleannieshop/backend/controller/EmailController.java
@@ -7,11 +7,15 @@ import com.cleannieshop.backend.service.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @RestController
+@RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:5173")
 public class EmailController {
     @Autowired
     private EmailService emailService;
